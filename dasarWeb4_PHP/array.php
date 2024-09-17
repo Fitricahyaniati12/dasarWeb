ce7d5bae456a866614bb7d1fd1fcd0938fac4a83
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-17 11:42:49 +0700

    Job4 -prak05

--- dasarWeb4_PHP/array.php
+++ dasarWeb4_PHP/array.php
@@ -26,4 +26,32 @@ foreach ($daftarkaryawan as $karyawan) {
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus) . "<br>";
 
 echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ', $karyawanPengalamanLimaTahun);
+
+// Daftar nilai per mata kuliah
+$daftarSiswa = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+];
+
+// Hitung jumlah nilai dan jumlah siswa
+$totalNilai = 0;
+$jumlahSiswa = count($daftarSiswa);
+
+foreach ($daftarSiswa as $siswa) {
+    $totalNilai += $siswa[1];
+}
+
+$averageNilai = $totalNilai / $jumlahSiswa;
+
+
+echo "<br>Daftar siswa yang memperoleh nilai di atas rata-rata ($averageNilai):<br>";
+
+foreach ($daftarSiswa as $siswa) {
+    if ($siswa[1] > $averageNilai) {
+        echo "Nama: {$siswa[0]}, Nilai: {$siswa[1]}<br>";
+    }
+}
 ?>
