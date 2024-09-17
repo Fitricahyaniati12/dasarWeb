1b936c4ffbefd8acfd08f3cfa57a4154ee5f43ca
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-17 10:45:50 +0700

    Job4 -prak04

--- dasarWeb4_PHP/strukturkontrol.php
+++ dasarWeb4_PHP/strukturkontrol.php
@@ -49,5 +49,19 @@ foreach ($skorUjian as $skor) {
 
 echo "<h2>Total Skor Ujian</h2>";
 echo "Total skor ujian adalah: $totalSkor";
+
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+
+echo "<h3>Penilaian Siswa</h3>";
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+
+    echo "Nilai: $nilai (Lulus) <br>";
+}
 ?>
 
