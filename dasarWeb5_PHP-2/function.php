9e338ce14f6f2c3824bc7b9a80e1f02977aed47c
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-24 10:20:40 +0700

    Modify function no12

--- dasarWeb5_PHP-2/function.php
+++ dasarWeb5_PHP-2/function.php
@@ -1,11 +1,19 @@
 <?php
-// Membuat fungsi
+// Membuat fungsi untuk menghitung umur
 function hitungUmur($thn_lahir, $thn_sekarang) {
-    $umur = $thn_sekarang - $thn_lahir; // Menghitung umur
-    return $umur; // Mengembalikan umur
+    return $thn_sekarang - $thn_lahir; 
 }
 
+// Membuat fungsi perkenalan
+function perkenalan($nama, $salam = "Assalamualaikum") {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>"; 
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Chya"); 
+
 // Menampilkan umur
-echo "Umur saya adalah " . hitungUmur(2005, 2024) . " tahun."; 
-// Ganti tahun lahir sesuai kebutuhan
+echo "Saya berusia " . hitungUmur(2005, 2024) . " tahun<br/>"; 
+echo "Senang berkenalan dengan Anda<br/>";
 ?>
