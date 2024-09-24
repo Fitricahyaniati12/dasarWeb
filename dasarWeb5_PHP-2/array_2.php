a2cec6984f59084627cbff6d5f3d4cffd927663d
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-24 08:55:55 +0700

    job5 -prak2 eksternal

--- dasarWeb5_PHP-2/array_2.php
+++ dasarWeb5_PHP-2/array_2.php
@@ -4,27 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Informasi Dosen</title>
-    <style>
-        table {
-            width: 50%;
-            border-collapse: collapse;
-            margin: 20px auto;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        tr:hover {
-            background-color: #f5f5f5;
-        }
-        h2 {
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to external CSS file -->
 </head>
 <body>
 <h2>Data Dosen</h2>
