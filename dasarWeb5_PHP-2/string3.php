36276105995cd4d7b3ffb9412eff0bb8d9bbf2bb
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-24 11:19:58 +0700

    Modify job5 prak7 string3

--- dasarWeb5_PHP-2/string3.php
+++ dasarWeb5_PHP-2/string3.php
@@ -1,6 +1,10 @@
 <?php
-
 $pesan = "saya arek blitar";
-echo strrev($pesan). "<br>";
-
+// Ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerkata = explode(" ", $pesan);
+// Ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerkata = array_map(fn($kata) => strrev($kata), $pesanPerkata);
+// Gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerkata);
+echo $pesan . "<br>";
 ?>
