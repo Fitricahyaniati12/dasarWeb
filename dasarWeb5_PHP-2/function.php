deba5a0c4ef148d82a7256eeddec27d309c66e3c
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-24 09:45:39 +0700

    job5 prak4

---
+++ dasarWeb5_PHP-2/function.php
@@ -0,0 +1,12 @@
+<?php
+function perkenalan() {
+    echo "Senang berkenalan dengan Anda!<br/>";
+}
+
+echo "Assalamualaikum, <br/>";
+echo "Perkenalkan, nama saya chya.<br/>"; 
+
+// Memanggil fungsi dua kali
+perkenalan();
+perkenalan();
+?>
