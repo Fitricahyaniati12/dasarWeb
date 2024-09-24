1c8b0f741e86d73236251b581f44f9c81f147e6f
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-09-24 08:52:01 +0700

    job5 -prak2 update

--- dasarWeb5_PHP-2/array_2.php
+++ dasarWeb5_PHP-2/array_2.php
@@ -4,18 +4,49 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Informasi Dosen</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+        h2 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-<?php
-$Dosen = [
-    'nama' => 'Elok Nur Hamdana',
-    'domisili' => 'Malang',
-    'jenis kelamin' => 'Perempuan'
-];
+<h2>Data Dosen</h2>
+<table>
+    <tr>
+        <th>Nama</th>
+        <th>Domisili</th>
+        <th>Jenis Kelamin</th>
+    </tr>
+    <tr>
+        <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis kelamin' => 'Perempuan'
+        ];
 
-echo "Nama: {$Dosen['nama']} <br>";
-echo "Domisili: {$Dosen['domisili']} <br>";
-echo "Jenis Kelamin: {$Dosen['jenis kelamin']} <br>";
-?>
+        echo "<td>{$Dosen['nama']}</td>";
+        echo "<td>{$Dosen['domisili']}</td>";
+        echo "<td>{$Dosen['jenis kelamin']}</td>";
+        ?>
+    </tr>
+</table>
 </body>
 </html>
