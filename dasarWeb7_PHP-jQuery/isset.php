e9bba658b3d28579dd1e70d9697a9c987727910c
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-10-08 09:27:03 +0700

    update isset

--- dasarWeb7_PHP-jQuery/isset.php
+++ dasarWeb7_PHP-jQuery/isset.php
@@ -1,8 +1,18 @@
 <?php
-$umur;
+/*$umur;
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa.";
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
+?>*/
+
+        //update
+
+$data = array("nama" => "Fitri", "usia" => 19);
+    if (isset($data["nama"])) {
+        echo "Nama: " . $data["nama"] . "<br>"; 
+    } else {
+         echo "Variabel 'nama' tidak ditemukan dalam array.<br>";
+    }
 ?>
