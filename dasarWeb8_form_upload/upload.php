e206782604b8ca06cc66c991d66950ed76bcbcc4
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-10-15 11:00:42 +0700

    update 2

--- dasarWeb8_form_upload/upload.php
+++ dasarWeb8_form_upload/upload.php
@@ -4,15 +4,18 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
+    // Ekstensi file yang diizinkan
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
     
-    // Ukuran maksimum file (5MB)
     $maxsize = 5 * 1024 * 1024;
 
+    // Periksa apakah file valid (ekstensi dan ukuran)
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         // Coba memindahkan file ke folder target
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br>";
+
+            echo "<img src='$targetfile' width='200' alt='Thumbnail'><br>";
         } else {
             echo "Gagal mengunggah file.";
         }
