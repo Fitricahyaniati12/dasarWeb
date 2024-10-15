3fefaa52fc18323fb7b53a65ebbeab882de5b204
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-10-15 11:16:31 +0700

    update 3

--- dasarWeb8_form_upload/upload.php
+++ dasarWeb8_form_upload/upload.php
@@ -4,23 +4,20 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    // Ekstensi file yang diizinkan
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    
-    $maxsize = 5 * 1024 * 1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+
+    $maxsize = 3 * 1024 * 1024;
 
     // Periksa apakah file valid (ekstensi dan ukuran)
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         // Coba memindahkan file ke folder target
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.<br>";
-
-            echo "<img src='$targetfile' width='200' alt='Thumbnail'><br>";
+            echo "File berhasil diunggah";
         } else {
             echo "Gagal mengunggah file.";
         }
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
