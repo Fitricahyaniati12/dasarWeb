2d1b7cf24a333c647cb541ec95400c1c52995aad
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-11-04 20:59:30 +0700

    Job10 Praktikum 3. CRUD Bagian Update

--- dasarWeb10_Crud/proses.php
+++ dasarWeb10_Crud/proses.php
@@ -1,24 +1,34 @@
 <?php
-include('koneksi.php');
+include('koneksi.php'); // Include database connection
 
-$aksi = $_GET['aksi'];
-$nama = $_POST['nama'];
-$jenis_kelamin = $_POST['jenis_kelamin']; // Changed variable name to use underscores
-$alamat = $_POST['alamat'];
-$no_telp = $_POST['no_telp'];
+$aksi = $_GET['aksi'] ?? '';
 
-if ($aksi == 'tambah') {
-    // Prepare the SQL query with the correct syntax
-    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
-    
-    if (mysqli_query($koneksi, $query)) {
-        header("Location: index.php");
-        exit();
+if ($aksi == 'ubah') {
+    if (isset($_POST['id'])) {
+        // Sanitize the ID and other inputs
+        $id = intval($_POST['id']);
+        $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+        $jenis_kelamin = mysqli_real_escape_string($koneksi, $_POST['jenis_kelamin']);
+        $alamat = mysqli_real_escape_string($koneksi, $_POST['alamat']);
+        $no_telp = mysqli_real_escape_string($koneksi, $_POST['no_telp']);
+
+        // Prepare the SQL query
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id='$id'";
+
+        // Execute the query
+        if (mysqli_query($koneksi, $query)) {
+            // Redirect on success
+            header("Location: index.php");
+            exit();
+        } else {
+            // Show error if the query fails
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
     } else {
-        // Provide feedback if the query fails
-        echo "Gagal menambahkan data: " . mysqli_error($koneksi);
+        echo "ID tidak valid.";
     }
 }
 
+// Close the database connection
 mysqli_close($koneksi);
 ?>
