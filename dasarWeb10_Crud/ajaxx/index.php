bfc616e3967725734f02fd0339c9065e2867e219
fitricahyaniati12 <fitricahyaniati4@gmail.com>
2024-11-15 23:07:07 +0700

    Praktikum 6. Tampilan CRUD dengan Ajax dan update index

--- dasarWeb10_Crud/ajaxx/index.php
+++ dasarWeb10_Crud/ajaxx/index.php
@@ -24,28 +24,32 @@ include 'auth.php';
         <h2 align="center" style="margin: 30px;">Data Anggota</h2>
         <form method="post" class="form-data" id="form-data">
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-9">
                     <div class="form-group">
                         <label>Nama</label>
                         <input type="hidden" name="id" id="id">
-                        <input type="text" name="nama" id="nama" class="form-control" required>
+                        <input type="text" name="nama" id="nama" class="form-control" required="true">
+                        <p class="text-danger" id="err_nama"></p>
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label>Jenis Kelamin</label><br>
-                        <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required> Laki-laki
+                        <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true"> Laki-laki
                         <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+                        <p class="text-danger" id="err_jenis_kelamin"></p>
                     </div>
                 </div>
             </div>
             <div class="form-group">
                 <label>Alamat</label>
-                <textarea name="alamat" id="alamat" class="form-control" required></textarea>
+                <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
+                <p class="text-danger" id="err_alamat"></p>
             </div>
             <div class="form-group">
                 <label>No Telepon</label>
-                <input type="number" name="no_telp" id="no_telp" class="form-control" required>
+                <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+                <p class="text-danger" id="err_no_telp"></p>
             </div>
             <div class="form-group">
                 <button type="button" name="simpan" id="simpan" class="btn btn-primary">
@@ -69,32 +73,66 @@ include 'auth.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
-            // Mengirimkan Token Keamanan
+            // Set up CSRF token in headers
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
                 }
             });
 
-            // Memuat data anggota
+            // Load data anggota
             $('.data').load("data.php");
 
-            // Simpan data anggota
-            $('#simpan').click(function() {
-                var data = $('#form-data').serialize();
-                $.ajax({
-                    url: 'form_action.php',
-                    type: 'POST',
-                    data: data,
-                    success: function(response) {
-                        alert('Data berhasil disimpan!');
-                        $('.data').load("data.php");
-                        $('#form-data')[0].reset();
-                    },
-                    error: function(xhr, status, error) {
-                        alert('Gagal menyimpan data: ' + error);
-                    }
-                });
+            // Click event for save button
+            $("#simpan").click(function() {
+                // Get form data
+                var data = $('.form-data').serialize();
+                var jenkell = document.getElementById("jenkel1").value;
+                var jenkel2 = document.getElementById("jenkel2").value;
+                var nama = document.getElementById("nama").value;
+                var alamat = document.getElementById("alamat").value;
+                var no_telp = document.getElementById("no_telp").value;
+
+                // Clear previous error messages
+                document.getElementById("err_nama").innerHTML = "";
+                document.getElementById("err_alamat").innerHTML = "";
+                document.getElementById("err_jenis_kelamin").innerHTML = "";
+                document.getElementById("err_no_telp").innerHTML = "";
+
+                // Validation checks
+                if (nama == "") {
+                    document.getElementById("err_nama").innerHTML = "Nama Harus Diisi";
+                }
+
+                if (alamat == "") {
+                    document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi";
+                }
+
+                if (document.getElementById("jenkel1").checked == false && document.getElementById("jenkel2").checked == false) {
+                    document.getElementById("err_jenis_kelamin").innerHTML = "Jenis Kelamin Harus Dipilih";
+                }
+
+                if (no_telp == "") {
+                    document.getElementById("err_no_telp").innerHTML = "No Telepon Harus Diisi";
+                }
+
+                // If all fields are valid, proceed with AJAX request
+                if (nama != "" && alamat != "" && (document.getElementById("jenkel1").checked == true || document.getElementById("jenkel2").checked == true) && no_telp != "") {
+                    $.ajax({
+                        type: 'POST',
+                        url: "form_action.php",
+                        data: data,
+                        success: function() {
+                            // Reload the data and reset the form
+                            $('.data').load("data.php");
+                            document.getElementById("id").value = "";
+                            document.getElementById("form-data").reset();
+                        },
+                        error: function(response) {
+                            console.log(response.responseText);
+                        }
+                    });
+                }
             });
         });
     </script>
